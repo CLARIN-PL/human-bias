fe7b528524ca128769386dca47770124f84292fc
Satyrr <gruzi12@gmail.com>
2021-08-07 16:31:21 +0200

    Add emotion dataset dirs to settings.py

--- personalized_nlp/settings.py
+++ personalized_nlp/settings.py
@@ -5,8 +5,8 @@ STORAGE_DIR = PROJECT_DIR / 'storage'
 LOGS_DIR = STORAGE_DIR / 'logs'
 CHECKPOINTS_DIR = STORAGE_DIR / 'checkpoints'
 
-EMBEDDINGS_DIR = STORAGE_DIR / 'embeddings'
-
+EMOTIONS_DATA = STORAGE_DIR / 'emotions_data'
+EMOTIONS_SIMPLE_DATA = STORAGE_DIR / 'emotions_simple_data'
 
 AGGRESSION_URL = 'https://ndownloader.figshare.com/articles/4267550/versions/5'
 ATTACK_URL = 'https://ndownloader.figshare.com/articles/4054689/versions/6'
